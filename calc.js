47d826849454fefe5c751a88ffefbbcbe3c3ac82
Nicholas Torkos <n.torkos@gmail.com>
2018-11-28 14:18:04 -0500

    calc changes, visual changes

--- calc.js
+++ calc.js
@@ -1,20 +1,24 @@
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function calcArea() {
 	var thickness = document.getElementById("thickness").value;
 	var width = document.getElementById("width").value;
 	var length = document.getElementById("length").value;
 
 	if (thickness && width && length) {
-		var area = ((width / 12) * length).toFixed(2);
-		var thermolbs = (area * thickness * 0.0106).toFixed(2);
+		var thermolbs = (((width / 12) * length) * thickness * 0.0098).toFixed(2);
 		var thermotons = (thermolbs / 2000).toFixed(2);
-		$(".thermo_tons").html(thermotons);
+		$(".thermo_tons").html(numberWithCommas(thermotons));
+		$(".thermo_lbs").html(numberWithCommas(thermolbs));
 		
 	} else {
-        $(".thermo_tons").html("--");
+        $(".thermo_tons, .thermo_lbs").html("--");
     }
 }
 
-$(".thermo_tons").html("--");
+$(".thermo_tons, .thermo_lbs").html("--");
 $('.ips').on('input', function (e) {
 	calcArea();
 });
